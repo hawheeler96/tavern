3071ace0b46af916a4bab0e2c5ad338b00967345
Holly Wheeler <hollyawheeler.96@gmail.com>
2024-02-07 14:11:02 -0800

    adds tailwind.css

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/CharacterSheet.js",
+    "./components/CreateCharacter.js",
+    "./components/EditCharacter.js",
+    "./components/Footer.js",
+    "./components/Header.js",
+    "./components/Home.js",
+    "./components/Login.js",
+    "./components/NavBar.js",
+    "./components/Parties.js",
+    "./components/Signup.js",
+    "./components/UserProfile.js",
+    "./components/ViewCharacters.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
