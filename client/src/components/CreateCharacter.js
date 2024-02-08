c3095a9bc0e246a3f05ba8aca6ae458595852151
Holly Wheeler <hollyawheeler.96@gmail.com>
2024-02-08 11:45:08 -0800

    edits POST for characters to include abscores and skills

--- client/src/components/CreateCharacter.js
+++ client/src/components/CreateCharacter.js
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from "react";
 
 function CreateCharacter() {
-
+    const [user_id, setUser_id] = useState("")
+    const [character_name, setCharacter_name] = useState("")
+    const [dnd_class, setDnd_class] = useState("")
+    const [subclasses, setSubclasses] = useState("")
+    const [hp, setHp] = useState("")
+    const [abilityscores_id, setAbilityscores_id] = useState("")
+    const [skills_id, setSkills_id] = useState("")
+    const [feats, setFeats] = useState("")
+    const [description, setDescription] = useState("")
+    const [background, setBackground] = useState("")
+    const [languages, setLanguages] = useState("")
+    const [gold, setGold] = useState("")
+    const [party_id, setParty_id] = useState("")
+    const [race_id, setRace_id] = useState("")
+    const [str_score, setStr_score] = useState("")
+    const [str_mod, setStr_mod] = useState("")
     return (
         <h1>Create your character</h1>
     )
