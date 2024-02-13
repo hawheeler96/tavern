f7870fb538446d34b32390949cf550d167c81d08
Holly Wheeler <hollyawheeler.96@gmail.com>
2024-02-13 15:24:47 -0800

    fixes server used

--- server/config.py
+++ server/config.py
@@ -24,6 +24,7 @@ app = Flask(
     template_folder="../client/build",
 )
 app.secret_key = environ.get("SECRET_KEY")
+
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
